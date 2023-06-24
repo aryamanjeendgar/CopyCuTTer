5af10ad229ee8fd06aba0a5a7575fec31d7605cd
Aryaman Jeendgar <jeendgararyaman@gmail.com>
2023-06-24 22:03:25 +0530

    Sensible input using the `EditableText` widget

---
+++ src/copycutter/EditableText.py
@@ -0,0 +1,118 @@
+# editabletext.py
+from textual.app import App, ComposeResult
+from textual.widgets import Button, Input, Label, Static
+
+class EditableText(Static):
+    """Custom widget to show (editable) static text."""
+
+    DEFAULT_CSS = """
+    EditableText {
+        layout: horizontal;
+        width: 1fr;
+        height: 3;
+    }
+
+    .editabletext--input {
+        width: 1fr;
+    }
+
+    .editabletext--label {
+        width: 1fr;
+        height: 3;
+        padding-left: 2;
+        border: round $primary;
+    }
+
+    .editabletext--edit {
+        box-sizing: border-box;
+        min-width: 0;
+        width: 4;
+    }
+
+    .editabletext--confirm {
+        box-sizing: border-box;
+        min-width: 0;
+        width: 4;
+    }
+
+    EditableText .ethidden {
+        display: none;
+    }
+    """
+
+    _confirm_button: Button
+    """The button to confirm changes to the text."""
+    _edit_button: Button
+    """The button to start editing the text."""
+    _input: Input
+    """The field that allows editing text."""
+    _label: Label
+    """The label that displays the text."""
+
+    def compose(self) -> ComposeResult:
+        self._input = Input(
+            placeholder="Type something...", classes="editabletext--input ethidden"
+        )
+        self._label = Label("", classes="editabletext--label")
+        self._edit_button = Button("📝", classes="editabletext--edit")
+        self._confirm_button = Button(
+            "✅", classes="editabletext--confirm ethidden", disabled=True
+        )
+
+        yield self._input
+        yield self._label
+        yield self._edit_button
+        yield self._confirm_button
+
+    @property
+    def is_editing(self) -> bool:  # The property `is_editing` checks if the internal `Input` widget is being shown or not.
+        """Is the text being edited?"""
+        return not self._input.has_class("ethidden")
+
+    @property
+    def value(self) -> str:
+       """Returns the current value in the Input widgets"""
+       return self._input.value
+
+    def on_button_pressed(self) -> None:  # When we press the edit/confirm button, we check in which mode we are in and switch to the other one.
+        if self.is_editing:
+            self.switch_to_display_mode()
+        else:
+            self.switch_to_editing_mode()
+
+    def switch_to_editing_mode(self) -> None:
+        if self.is_editing:
+            return
+
+        self._input.value = str(self._label.renderable)
+
+        self._label.add_class("ethidden")
+        self._input.remove_class("ethidden")
+
+        self._edit_button.disabled = True
+        self._edit_button.add_class("ethidden")
+        self._confirm_button.disabled = False
+        self._confirm_button.remove_class("ethidden")
+
+    def switch_to_display_mode(self) -> None:
+        if not self.is_editing:
+            return
+
+        self._label.renderable = self._input.value
+
+        self._input.add_class("ethidden")
+        self._label.remove_class("ethidden")
+
+        self._confirm_button.disabled = True
+        self._confirm_button.add_class("ethidden")
+        self._edit_button.disabled = False
+        self._edit_button.remove_class("ethidden")
+
+class EditableTextApp(App[None]):
+    def compose(self) -> ComposeResult:
+        yield EditableText()
+
+app = EditableTextApp()
+
+if __name__ == "__main__":
+    app.run()
--- src/copycutter/TextQuestion.py
+++ src/copycutter/TextQuestion.py
@@ -1,32 +1,42 @@
 #!/usr/bin/env python3
 from textual.app import App, ComposeResult
-from textual.widgets import Input, Footer, Static
-
+from textual.containers import Container
+from textual.widgets import Footer, Static, Label
+from EditableText import EditableText
 
 class TextQuestion(Static):
-    _input_1: Input
-    _input_2: Input
+    _label_1: Label
+    _label_2: Label
+    _input_1: EditableText
+    _input_2: EditableText
 
+    def compose(self) -> ComposeResult:
+        self._label_1 = Label("This is a fancy description")
+        self._input_1 = EditableText()
+        self._label_2 = Label("This is another fancy description")
+        self._input_2 = EditableText()
+        yield Container(
+            self._label_1,
+            self._input_1,
+            self._label_2,
+            self._input_2,
+        )
+        yield Footer()
+
+class TestApp(App):
     BINDINGS = [
         ('h', 'dump_values', 'Dump Values')
     ]
+    _textbox: TextQuestion
     def compose(self) -> ComposeResult:
-        self._input_1 = Input(placeholder="First Name")
-        self._input_2 = Input(placeholder="Last Name")
-
-        yield self._input_1
-        yield self._input_2
-        yield Footer()
+        self._textbox = TextQuestion()
+        yield self._textbox
 
     def action_dump_values(self) -> None:
-        l1 = self._input_1.value
-        l2 = self._input_2.value
+        l1 = self._textbox._input_1.value
+        l2 = self._textbox._input_2.value
         with open('test.txt', 'w') as op_file:
             op_file.write("First Name: {}\nLast Name: {}".format(l1, l2))
-
-class TestApp(App):
-    def compose(self) -> ComposeResult:
-        yield TextQuestion()
 
 if __name__ == "__main__":
     app = TestApp()
