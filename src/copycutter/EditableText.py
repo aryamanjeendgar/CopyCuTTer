dd4869c3978643f26454a2739057978b711f5b12
Aryaman Jeendgar <jeendgararyaman@gmail.com>
2023-06-24 14:50:31 +0530

    Porting over some stuff from TODO example

---
+++ src/copycutter/EditableText.py
@@ -0,0 +1,139 @@
+# editabletext.py
+from __future__ import annotations
+
+from textual.app import App, ComposeResult
+from textual.message import Message
+from textual.widgets import Button, Input, Label, Static
+
+class EditableText(Static):
+    """Custom widget to show (editable) static text."""
+
+    DEFAULT_CSS = """
+    EditableText {
+        layout: horizontal;
+        width: 1fr;
+        height: 3;
+    }
+
+    .editabletext--input {
+        width: 1fr;
+        height: 3;
+    }
+
+    .editabletext--label {
+        width: 1fr;
+        height: 3;
+        border: round $primary;
+    }
+
+    .editabletext--edit {
+        min-width: 0;
+        width: 4;
+    }
+
+    .editabletext--confirm {
+        min-width: 0;
+        width: 4;
+    }
+
+    EditableText .ethidden {
+        display: none;
+    }
+    """
+
+    _confirm_button: Button
+    """The button to confirm changes to the text."""
+    _edit_button: Button
+    """The button to start editing the text."""
+    _input: Input
+    """The field that allows editing text."""
+    _label: Label
+    """The label that displays the text."""
+
+    class Display(Message):  # Posted when the widget switches to display mode.
+        """The user switched to display mode."""
+
+        editable_text: EditableText
+        """The EditableText instance that changed into display mode."""
+
+        def __init__(self, editable_text: EditableText) -> None:
+            self.editable_text = editable_text
+            super().__init__()
+
+    class Edit(Message):  # Posted when the widget switches to edit mode.
+        """The user switched to edit mode."""
+
+        editable_text: EditableText
+        """The EditableText instance that changed into edit mode."""
+
+        def __init__(self, editable_text: EditableText) -> None:
+            self.editable_text = editable_text
+            super().__init__()
+
+    def compose(self) -> ComposeResult:
+        self._input = Input(
+            placeholder="Type something...", classes="editabletext--input ethidden"
+        )
+        self._label = Label("", classes="editabletext--label")
+        self._edit_button = Button("📝", classes="editabletext--edit")
+        self._confirm_button = Button(
+            "✅", classes="editabletext--confirm ethidden", disabled=True
+        )
+
+        yield self._input
+        yield self._label
+        yield self._edit_button
+        yield self._confirm_button
+
+    @property
+    def is_editing(self) -> bool:
+        """Is the text being edited?"""
+        return not self._input.has_class("ethidden")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        event.stop()  # We prevent the message `Button.Pressed` from bubbling up the DOM hierarchy.
+        if self.is_editing:
+            self.switch_to_display_mode()
+        else:
+            self.switch_to_editing_mode()
+
+    def switch_to_editing_mode(self) -> None:
+        if self.is_editing:
+            return
+
+        self._input.value = str(self._label.renderable)
+
+        self._label.add_class("ethidden")
+        self._input.remove_class("ethidden")
+
+        self._edit_button.disabled = True
+        self._edit_button.add_class("ethidden")
+        self._confirm_button.disabled = False
+        self._confirm_button.remove_class("ethidden")
+
+        self.post_message(self.Edit())  # We post this so that container apps / screens can react appropriately.
+
+    def switch_to_display_mode(self) -> None:
+        if not self.is_editing:
+            return
+
+        self._label.renderable = self._input.value
+
+        self._input.add_class("ethidden")
+        self._label.remove_class("ethidden")
+
+        self._confirm_button.disabled = True
+        self._confirm_button.add_class("ethidden")
+        self._edit_button.disabled = False
+        self._edit_button.remove_class("ethidden")
+
+        self.post_message(self.Display())  # We post this so that container apps / screens can react appropriately.
+
+class EditableTextApp(App[None]):
+    def compose(self) -> ComposeResult:
+        yield EditableText()
+
+app = EditableTextApp()
+
+if __name__ == "__main__":
+    app.run()
