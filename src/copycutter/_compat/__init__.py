25519f36c3852da32990626548dc11ea7c0fa671
Henry Schreiner <henryschreineriii@gmail.com>
2023-08-11 15:40:56 -0400

    chore: remove unused compat code
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/copycutter/_compat/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Copyright (c) 2023 Aryaman Jeendgar. All rights reserved.
-
-CopyCuTTer: A TUI for the `Scientific-Python/cookie` and `Copier` projects
-"""
-
-
-from __future__ import annotations
